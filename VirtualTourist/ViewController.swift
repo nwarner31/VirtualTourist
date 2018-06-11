01b0918955086cb0f11aef9e0feaf0079efb41d4
Nathaniel Warner <natedawg3124@gmail.com>
2018-06-10 22:32:22 -0700

    initial commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Nathaniel Warner on 6/10/18.
+//  Copyright © 2018 Nathaniel Warner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
